1a4399f0a0482826bc0521a368a5ea9561fdf06e
sbyte <stanislsv.bajt@score-alarm.com>
2025-07-20 15:39:15 +0200

    Limit text field input to numbers
    The text field in the `InputSection` now only accepts numeric input.

--- app/src/main/java/com/sbajt/matscounter/ui/composables/InputSection.kt
+++ app/src/main/java/com/sbajt/matscounter/ui/composables/InputSection.kt
@@ -2,11 +2,12 @@ package com.sbajt.matscounter.ui.composables
 
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.sbajt.matscounter.ui.models.InputSectionUiState
@@ -21,12 +22,15 @@ fun InputSection(
     Box(
         modifier = modifier.padding(16.dp)
     ) {
+        val currentCount = uiState?.itemCount?.toString() ?: "0"
         TextField(
             modifier = modifier,
             label = {
-                Text(text = remember { "${uiState?.selectedItem?.name ?: "-"} count" })
+                Text(text = "${uiState?.selectedItem?.name ?: "-"} count")
             },
-            value = remember { uiState?.itemCount?.toString() ?: "0" },
+            value = currentCount,
+            singleLine = true,
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
             onValueChange = { newItemCount ->
                 val filteredInput = newItemCount.filter { it.isDigit() }
                 val newCount = filteredInput.toIntOrNull() ?: 0
