639ca5ef0420f36d23b08eae3ac6c032720a3fdd
sbyte <stanko.bajt@gmail.com>
2025-09-12 16:20:39 +0200

    Refactor: Add fading edges to `ItemBuildpathScreen`
    This commit enhances the `ItemBuildpathScreen` by adding fading edges to the top and bottom of the `LazyColumn` for improved visual appeal when scrolling.
    
    Key changes:
    - In `ItemBuildpathScreen.kt`, within the `ContentScreen` composable:
        - Applied `graphicsLayer { alpha = 0.99f }` to the `LazyColumn` to enable blending.
        - Used the `drawWithContent` modifier to draw a vertical gradient at the top and bottom of the `LazyColumn`.
        - The gradient transitions from the `MatsCounterTheme.colors.background` to `Color.Transparent` and back, creating the fading effect.
        - The height of the fading edge is determined by `MatsCounterTheme.dimensions.medium`.
        - `BlendMode.DstIn` is used to ensure the gradient only affects the content within the `LazyColumn`.

--- app/src/main/java/com/sbajt/matscounter/ui/composables/screens/ItemBuildpathScreen.kt
+++ app/src/main/java/com/sbajt/matscounter/ui/composables/screens/ItemBuildpathScreen.kt
@@ -12,6 +12,12 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.drawWithContent
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.BlendMode
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.tooling.preview.PreviewLightDark
 import androidx.compose.ui.tooling.preview.PreviewParameter
 import com.sbajt.matscounter.ui.composables.previewProviders.ItemBuildPathUiStateProvider
@@ -41,55 +47,77 @@ fun ItemBuildPathScreen(
 fun ContentScreen(
     uiState: ItemBuildPathScreenUiState.Content,
     modifier: Modifier,
-) = LazyColumn(
-    modifier = Modifier
-        .fillMaxSize()
-        .background(MatsCounterTheme.colors.background)
-        .padding(MatsCounterTheme.dimensions.contentPadding)
 ) {
-    item {
-        Row(
-            modifier = modifier
-                .background(MatsCounterTheme.colors.background)
-                .fillMaxWidth(),
-            horizontalArrangement = Arrangement.spacedBy(
-                MatsCounterTheme.dimensions.medium,
-                Alignment.CenterHorizontally
-            ),
-            verticalAlignment = Alignment.CenterVertically
-        ) {
-            ItemView(
-                modifier = Modifier.padding(vertical = MatsCounterTheme.dimensions.medium),
-                uiState = uiState.selectedItem,
-                onItemSelected = { _, _ -> },
-            )
-            Text(
-                style = MatsCounterTheme.typography.subtitleTextNormal,
-                color = MatsCounterTheme.colors.primary,
-                text = remember { "x${uiState.selectedItemAmount}" },
-            )
-        }
-    }
-
-    if (uiState.selectedItemBuildMaterialListWrapperList.isNotEmpty()) {
-        uiState.selectedItemBuildMaterialListWrapperList.forEachIndexed { index, buildMaterialListWrapper ->
-            item {
-                Arrow()
-            }
-            item("group_$index") {
+    val backGroundColor = MatsCounterTheme.colors.background
+    val fadingEdgeHeight = MatsCounterTheme.dimensions.medium
+    LazyColumn(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(MatsCounterTheme.colors.background)
+            .padding(MatsCounterTheme.dimensions.contentPadding)
+            .graphicsLayer { alpha = 0.99f }
+            .drawWithContent {
+                val colors = listOf(backGroundColor, Color.Transparent)
+                drawContent()
+                //draw top fading edge
+                drawRect(
+                    size = size.copy(height = fadingEdgeHeight.toPx()),
+                    brush = Brush.verticalGradient(colors),
+                    blendMode = BlendMode.DstIn
+                )
+                //draw bottom fading edge
+                drawRect(
+                    topLeft = Offset(x= 0f, size.copy(height = fadingEdgeHeight.toPx()).height),
+                    size = size.copy(height = fadingEdgeHeight.toPx()),
+                    brush = Brush.verticalGradient(colors.reversed()),
+                    blendMode = BlendMode.DstIn
+                )
+            },
+    ) {
+        item {
+            Row(
+                modifier = modifier
+                    .background(MatsCounterTheme.colors.background)
+                    .fillMaxWidth(),
+                horizontalArrangement = Arrangement.spacedBy(
+                    MatsCounterTheme.dimensions.medium,
+                    Alignment.CenterHorizontally
+                ),
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                ItemView(
+                    modifier = Modifier.padding(vertical = MatsCounterTheme.dimensions.medium),
+                    uiState = uiState.selectedItem,
+                    onItemSelected = { _, _ -> },
+                )
                 Text(
-                    modifier = Modifier.padding(bottom = MatsCounterTheme.dimensions.small),
-                    style = MatsCounterTheme.typography.titleTextNormal,
+                    style = MatsCounterTheme.typography.subtitleTextNormal,
                     color = MatsCounterTheme.colors.primary,
-                    text = buildMaterialListWrapper.titleText ?: ""
+                    text = remember { "x${uiState.selectedItemAmount}" },
                 )
             }
-            buildMaterialListWrapper.buildMaterialsList.forEach { buildMaterial ->
-                item("group_${index}_item_${buildMaterial.name}") {
-                    BuildMaterialView(
-                        modifier = Modifier.padding(start = MatsCounterTheme.dimensions.medium),
-                        uiState = buildMaterial,
+        }
+
+        if (uiState.selectedItemBuildMaterialListWrapperList.isNotEmpty()) {
+            uiState.selectedItemBuildMaterialListWrapperList.forEachIndexed { index, buildMaterialListWrapper ->
+                item {
+                    Arrow()
+                }
+                item("group_$index") {
+                    Text(
+                        modifier = Modifier.padding(bottom = MatsCounterTheme.dimensions.small),
+                        style = MatsCounterTheme.typography.titleTextNormal,
+                        color = MatsCounterTheme.colors.primary,
+                        text = buildMaterialListWrapper.titleText ?: ""
                     )
+                }
+                buildMaterialListWrapper.buildMaterialsList.forEach { buildMaterial ->
+                    item("group_${index}_item_${buildMaterial.name}") {
+                        BuildMaterialView(
+                            modifier = Modifier.padding(start = MatsCounterTheme.dimensions.medium),
+                            uiState = buildMaterial,
+                        )
+                    }
                 }
             }
         }
